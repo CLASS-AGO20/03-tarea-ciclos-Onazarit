b9c5049111071620c4841563aaf294707b3f33dd
Onazarit <onazarit@ucol.mx>
2020-12-11 12:50:05 -0600

    Implemented function sumatoriaSerieDos

--- src/main.js
+++ src/main.js
@@ -6,9 +6,29 @@ export default class App {
         }
         return(resultado);
     }
+
+    sumatoriaSerieDos(numero){
+        let resultado = 1, i=2,opc=true;
+        while(i<=numero){
+            if(opc == true){
+                resultado = resultado + (1/i);
+                opc = false;
+            }
+            else{
+                resultado = resultado - (1/i);
+                opc=true;
+            }
+            i++;
+        }
+        return(resultado);
+    }
 }
 
 let app = new App();
 // Prueba metodo sumatoriaSerieUno
 console.log("Sumatoria de 6: "+app.sumatoriaSerieUno(6));
 console.log("Sumatoria de 2: "+app.sumatoriaSerieUno(2));
+
+// Prueba metodo sumatoriaSerieDos
+console.log("Sumatoria de 6: "+app.sumatoriaSerieDos(6));
+console.log("Sumatoria de 2: "+app.sumatoriaSerieDos(2));
