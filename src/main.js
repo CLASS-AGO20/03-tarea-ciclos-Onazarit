fec56e2d54036044bd0c24d17599869dff28bd51
Onazarit <onazarit@ucol.mx>
2020-12-11 12:40:42 -0600

    Implemented function sumatoriaSerieUno

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+    sumatoriaSerieUno(numero){
+        let resultado = 0;
+        for(let i=1;i<=numero;i++){
+            resultado = resultado + (1/i);
+        }
+        return(resultado);
+    }
+}
 
 let app = new App();
+// Prueba metodo sumatoriaSerieUno
+console.log("Sumatoria de 6: "+app.sumatoriaSerieUno(6));
+console.log("Sumatoria de 2: "+app.sumatoriaSerieUno(2));
